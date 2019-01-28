7774e7a62a34730fe92fe478c1e7b998265c4fa6
edouardLaffont <edouard.laffont@live.com>
2019-01-28 20:10:57 +0100

    Création d'une nouvelle Class d'héritage de Product : Fridge.

--- src/com/cursan/homeshop/Product.java
+++ src/com/cursan/homeshop/Product.java
@@ -1,9 +1,9 @@
 package com.cursan.homeshop;
 
 public class Product {
-    String name;
-    String description;
-    double price;
+    private String name;
+    private String description;
+    private double price;
 
     public Product(String name, String description, double price) {
         this.name = name;
