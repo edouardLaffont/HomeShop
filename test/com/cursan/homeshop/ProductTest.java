99e9e7b18ed54753a58b5336eb9aea0b475445c0
edouardLaffont <edouard.laffont@live.com>
2019-02-01 15:51:14 +0100

    Création de tests pour les nouvelles fonctions de livraison

--- test/com/cursan/homeshop/ProductTest.java
+++ test/com/cursan/homeshop/ProductTest.java
@@ -1,7 +1,41 @@
 package com.cursan.homeshop;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ProductTest {
+public class RelayDeliveryTest {
 
+    @Test
+    public void Given_FreeRelay_WhenGettingDeliveryPrice_ThenGet0e() {
+        Delivery delivery = new RelayDelivery(5);
+        assertEquals(0.0, delivery.getPrice(), 0.01);
+    }
+
+    @Test
+    public void Given_LowPriceRelay_WhenGettingDeliveryPrice_ThenGet2e99() {
+        Delivery delivery = new RelayDelivery(27);
+        assertEquals(2.99, delivery.getPrice(), 0.01);
+    }
+
+    @Test
+    public void Given_HighPriceRelay_WhenGettingDeliveryPrice_ThenGet4e99() {
+        Delivery delivery = new RelayDelivery(52);
+        assertEquals(4.99, delivery.getPrice(), 0.01);
+    }
+
+
+    public class ExpressDeliveryTest {
+        @Test
+        public void Given_RegionCityAsLocation_WhenGettingDeliveryPrice_ThenGet9e99() {
+            Delivery delivery = new ExpressDelivery("Bordeaux");
+            assertEquals(9.99, delivery.getPrice(), 0.01);
+        }
+        @Test
+        public void Given_ParisAslocation_WhenGettingDeliveryPrice_ThenGet6e99() {
+            Delivery delivery = new ExpressDelivery("Paris");
+            assertEquals(6.99, delivery.getPrice(), 0.01);
+        }
+
+    }
 }
+
