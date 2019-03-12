271f47231cfa8b7cce1b018cf1dd6f75f2fe051d
edouardLaffont <edouard.laffont@live.com>
2019-03-12 11:24:20 +0100

    Ajout de Maven, mise à jour du projet et ajout de la version 3.1 de Servlet.

--- src/com/cursan/homeshop/Delivery.java
+++
@@ -1,7 +0,0 @@
-package com.cursan.homeshop;
-
-public interface Delivery {
-    public double getPrice();
-
-    String getInfo();
-}
--- src/com/cursan/homeshop/NoProductInBillException.java
+++
@@ -1,4 +0,0 @@
-package com.cursan.homeshop;
-
-public class NoProductInBillException extends RuntimeException {
-}
