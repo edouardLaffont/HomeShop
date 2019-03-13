3e2dd0622ec90779859f8f9142df3bb363cadf9b
edouardLaffont <edouard.laffont@live.com>
2019-03-13 14:37:31 +0100

    Instalation et configuration du plugin Eclipse Jetty création du fichier web.xml

---
+++ src/main/java/com/cursan/homeshop/com/cursan/homeshop/web/HelloWorldServlet.java
@@ -0,0 +1,14 @@
+package com.cursan.homeshop.com.cursan.homeshop.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello OpenClassrooms !");
+    }
+}
